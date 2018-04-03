6a57d5c47eb6eafd0f0595043d9544621c9ae5e3
zhiweiwong96 <zhiwei1996@1utar.my>
2018-04-03 13:27:23 +0800

    modified login.php

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "";
+	$dbname = "a3mldb";
+
+	// Create connection
+	$conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	echo "Connected successfully";
+
+?>
--- login.php
+++ login.php
@@ -1,77 +1,61 @@
-<!doctype html>  
-<html>  
-<head>  
-<title>Login</title>  
-<style>   
-body{  
-              
-    margin-top: 100px;  
-    margin-bottom: 100px;  
-    margin-right: 150px;  
-    margin-left: 80px;  
-    background-color: azure ;  
-    color: palevioletred;  
-    font-family: verdana;  
-    font-size: 100%  
-      
-}  
-h1 {  
-    color: indigo;  
-    font-family: verdana;  
-    font-size: 100%;  
-}  
-h3 {  
-    color: indigo;  
-    font-family: verdana;  
-    font-size: 100%;  
-} </style>  
-</head>  
-<body>  
-     <center><h1>CREATE REGISTRATION AND LOGIN FORM USING PHP AND MYSQL</h1></center>  
-   <p><a href="register.php">Register</a> | <a href="login.php">Login</a></p>  
-<h3>Login Form</h3>  
-<form action="" method="POST">  
-Username: <input type="text" name="user"><br />  
-Password: <input type="password" name="pass"><br />   
-<input type="submit" value="Login" name="submit" />  
-</form>  
-<?php  
-if(isset($_POST["submit"]))
-{  
-  
-if(!empty($_POST['user']) && !empty($_POST['pass'])) {  
-    $user=$_POST['user'];  
-    $pass=$_POST['pass'];  
-  
-    $con=mysql_connect('localhost','root','') or die(mysql_error());  
-    mysql_select_db'user_registration') or die("cannot select DB");  
-  
-    $query=mysql_query("SELECT * FROM login WHERE username='".$user."' AND password='".$pass."'");  
-    $numrows=mysql_num_rows($query);  
-    if($numrows!=0)  
-    {  
-    while($row=mysql_fetch_assoc($query))  
-    {  
-    $dbusername=$row['username'];  
-    $dbpassword=$row['password'];  
-    }  
-  
-    if($user == $dbusername && $pass == $dbpassword)  
-    {  
-    session_start();  
-    $_SESSION['sess_user']=$user;  
-  
-    /* Redirect browser */  
-    header("Location: member.php");  
-    }  
-    } else {  
-    echo "Invalid username or password!";  
-    }  
-  
-} else {  
-    echo "All fields are required!";  
-}  
-}  
-?>  
-</body>  
-</html> 
+<!doctype html>
+<html>
+<head>
+<title>Login</title>
+<style>
+body{
+
+    margin-top: 100px;
+    margin-bottom: 100px;
+    margin-right: 150px;
+    margin-left: 80px;
+    background-color: azure ;
+    font-family: verdana;
+    font-size: 100%
+}
+h3 {
+    color: indigo;
+    font-family: verdana;
+    font-size: 100%;
+}
+form{
+
+    font-family: verdana;
+    text-align: center;
+
+}
+
+</style>
+</head>
+<script>
+
+  function validationform(){
+    var username = document.loginform.user.value;
+    var password = document.loginform.pass.value;
+
+    if(username==null || username==""){
+      alert("Invalid Username Input");
+      return false;
+    }else if (password==null || password=="") {
+      alert("Invalid Password Input");
+      return false;
+    }
+  }
+
+</script>
+<body>
+     <center><h1>LOGIN</h1></center>
+
+<form name="loginform" action="" method="POST" onsubmit="return validationform()">
+
+  Username: <br>
+  <input type="text" name="user" placeholder="Enter your username"><br>
+  Password: <br>
+   <input type="password" name="pass" placeholder="Enter your password"><br>
+
+<input type="submit" value="Login" name="Login">
+</form>
+
+
+</body>
+</html>
