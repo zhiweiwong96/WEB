0cbc53a17393e398b95a1b77595195e5cb452e84
zhiweiwong96 <zhiwei1996@1utar.my>
2018-04-03 09:34:50 +0800

    modify sql file

--- a3mldb.sql
+++ a3mldb.sql
@@ -1,8 +1,17 @@
-create table login(
+create table admin(
     username varchar(200) not null,
     password varchar(200) not null,
     primary key (username)
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) engine = InnoDB;
 
-insert into login
+insert into admin
 values("admin","123456");
+
+create table video (
+    video_id int not null auto_increment primary key,
+    title varchar(100) not null,
+    year int not null,
+    genre varchar(100) not null,
+    img_path varchar(300) not null,
+    synopsis varchar(1000)
+) engine = InnoDB;
